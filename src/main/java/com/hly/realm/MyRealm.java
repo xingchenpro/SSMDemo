dd683ccea371825f9a36862a445940781e584e7d
黄良运 <1136513099@qq.com>
2018-08-29 21:09:23 +0800

    login

---
+++ src/main/java/com/hly/realm/MyRealm.java
@@ -0,0 +1,28 @@
+package com.hly.realm;
+
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+
+/**
+ * @author :hly
+ * @github :github.com/SiriusHly
+ * @blog :blog.csdn.net/Sirius_hly
+ * @date :2018/8/29
+ */
+public class MyRealm extends AuthorizingRealm {
+
+
+    @Override
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
+        return null;
+    }
+
+    @Override
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
+        return null;
+    }
+}
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,18 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: hly
+  Date: 2018/8/29
+  Time: 20:37
+  github :github.com/SiriusHly
+  blog :blog.csdn.net/Sirius_hly
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
+<head>
+    <title>index</title>
+</head>
 <body>
-<h2>Hello World!</h2>
+Exploiting
 </body>
 </html>
